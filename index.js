6bcbe2ba8075cbd4274eb210b98f3f7091c4a9fc
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-16 09:29:49 -0400

    ISS Spotter III

---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
